a3d8cb5ef80d50f7145f728133874ca418760878
Yun,HeeKyung <hk11508@hanmail.net>
2021-05-21 17:23:42 +0900

    20210521 첫번째 포트폴리오

---
+++ src/com/hkyun/cafe/Beverage.java
@@ -0,0 +1,5 @@
+package com.hkyun.cafe;
+
+public enum Beverage {
+      민트초코라떼,감식초,오이큐브라떼,토마토쥬스,건포도스무디
+}
---
+++ src/com/hkyun/cafe/Cafe.java
@@ -0,0 +1,11 @@
+package com.hkyun.cafe;
+
+public class Cafe {
+
+	public static void main(String[] args) {
+		Beverage menu = Beverage.민트초코라떼;
+		System.out.println("밸런스 최강 호불호 카페에 오신것을 환영합니다^__^");
+		System.out.println(Beverage.감식초);
+	}
+
+}
